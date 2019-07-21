2a0d202a7edb3efb8b315990eefd64f39542bacd
Level.Z <1171567018@qq.com>
2019-07-21 16:09:36 +0800

    go demo

--- 13.multiple_return_value.go
+++ 13.multiple_return_value.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func modifyArray(a [3]string) [3]string {
+	a[1] = "x"
+	return a
+}
+
+func modifySlice(a []string) []string {
+	a[1] = "i"
+	return a
+}
+
+func modifyComplexArray(a [3][]string) [3][]string {
+	a[1][1] = "s"
+	a[2] = []string{"o", "p", "q"}
+	return a
+}
+
 // 抛出多值
 func getValues ()(int, int) {
   return 3, 7 
@@ -16,4 +32,31 @@ func main () {
   // 使用_忽略不想要的值
   _, value3 := getValues()
   fmt.Println(value3)
+
+  array1 := [3]string{"a", "b", "c"}
+	fmt.Printf("The array: %v\n", array1)
+	array2 := modifyArray(array1)
+  // array1 的值不会被改变
+	fmt.Printf("The modified array: %v\n", array2)
+	fmt.Printf("The original array: %v\n", array1)
+	fmt.Println()
+
+  slice1 := []string{"x", "y", "z"}
+	fmt.Printf("The slice: %v\n", slice1)
+	slice2 := modifySlice(slice1)
+  // slice1 的值会被改变
+	fmt.Printf("The modified slice: %v\n", slice2)
+	fmt.Printf("The original slice: %v\n", slice1)
+	fmt.Println()
+
+  // complexArray1 的值会被改变
+	complexArray1 := [3][]string{
+		[]string{"d", "e", "f"},
+		[]string{"g", "h", "i"},
+		[]string{"j", "k", "l"},
+	}
+	fmt.Printf("The complex array: %v\n", complexArray1)
+	complexArray2 := modifyComplexArray(complexArray1)
+	fmt.Printf("The modified complex array: %v\n", complexArray2)
+	fmt.Printf("The original complex array: %v\n", complexArray1)
 }
--- README.md
+++ README.md
@@ -23,16 +23,16 @@
 |19|[method](./19.method.go)|方法属性|Y|
 |20|[interface](./20.interface.go)|接口|Y|
 |21|[error](./21.error.go)|错误处理|Y|
-|22|[goroutine](./22.goroutine.go)|goroutine|Y|
+|22|[goroutine](./22.goroutine.go)|go程|Y|
 |23|[channel](./23.channel.go)|通道|Y|
 |24|[channel buffer](./24.channel_buffer.go)|缓冲通道|Y|
 |25|[channel sync](./25.channel_sync.go)|非缓冲通道|Y|
-|26|[channel direction](./26.channel_direction.go)|管道约束|Y|
-|27|[select](./27.select.go)|多重管道|Y|
+|26|[channel direction](./26.channel_direction.go)|通道约束|Y|
+|27|[select](./27.select.go)|多重通道|Y|
 |28|[timeout](./28.timeout.go)|超时|Y|
-|29|[non block channel operation](./29.non_block_channel_operation.go)|无阻塞管道|Y|
-|30|[close channel](./30.close_channel.go)|关闭管道|Y|
-|31|[range channel](./31.range_channel.go)|管道范围|Y|
+|29|[non block channel operation](./29.non_block_channel_operation.go)|无阻塞通道|Y|
+|30|[close channel](./30.close_channel.go)|关闭通道|Y|
+|31|[range channel](./31.range_channel.go)|通道范围|Y|
 |32|[timer](./32.timer.go)|计时器|Y|
 |33|[ticker](./33.ticker.go)|定时器|Y|
 |34|[worker pool](./34.worker_pool.go)|工作池|Y|
