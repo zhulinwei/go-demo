b50e94e4a147f0507d8227c029459c73438cf9fa
Level.Z <1171567018@qq.com>
2019-07-21 12:55:42 +0800

    go demo

--- 24.channel_buffer.go
+++ 24.channel_buffer.go
@@ -4,13 +4,33 @@ import "fmt"
 
 func main () {
   // 初始化一个channel并设置可缓存2个值
-  messages := make(chan string, 2)
+  messages1 := make(chan string, 2)
+  messages1 <- "test1"
+  messages1 <- "test2"
+  fmt.Println(<-messages1)
+  fmt.Println(<-messages1)
 
-  messages <- "test1"
-  messages <- "test2"
+  messages2 := make(chan int, 2)
+  // 发送
+  go func () {
+    for i := 0; i < 10; i++ {
+      fmt.Println("send:", i)
+      messages2 <- i
+    } 
+    fmt.Println("close channel")
+    close(messages2)
+  }()
 
-  fmt.Println(<-messages)
-  fmt.Println(<-messages)
+  // 接收
+  for {
+    elem, ok := <-messages2
+    if !ok {
+      fmt.Println("channel had closed")
+      break
+    }
+    fmt.Println("receive:", elem)
+  }
+  fmt.Println("end!")
 }
 
 
